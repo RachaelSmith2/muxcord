5042b4647d6c6986a662f42ba5bde4dd6f90f82c
Dewan M.I. Mukto <dewanmukto@gmail.com>
2021-10-29 21:55:00 -0230

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {};
 
-config.port = process.env.PORT || 5000;
-config.dbURL = process.env.DATABASEURL || "mongodb://localhost/minicord";
+
+config.port = process.env.PORT || 3000;
+config.dbURL = process.env.DATABASEURL || "mongodb+srv://new-user-001:YouSawThis@cluster0.vn8jv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 module.exports = config;
