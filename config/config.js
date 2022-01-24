32f245382e363a6bcb740567dc1120b7dc121b31
Rachael <85811185+RachaelSmith2@users.noreply.github.com>
2022-01-24 17:17:57 +1100

    commit

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const config = {};
 
 
 config.port = process.env.PORT || 3000;
-config.dbURL = process.env.DATABASEURL || "mongodb+srv://"; // enter your MongoDB URI here
+config.dbURL = process.env.DATABASEURL || "mongodb+srv://admina:fefe@cluster9.eczki.mongodb.net/cluster9?retryWrites=true&w=majority"; // enter your MongoDB URI here
 
 module.exports = config;
